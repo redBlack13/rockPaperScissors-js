696e77becd0d59f841a9658aacc6585c9fff703d
Red Black13 <149127977+redBlack13@users.noreply.github.com>
2023-12-30 19:22:01 +1100

    outlined basic frameworks/design for game computer choice function completed

---
+++ rockPaperScissors.js
@@ -0,0 +1,19 @@
+ const choices = ["rock", "paper", "scissors"]
+ 
+ function game () {
+    //play the gamer
+    //play 5 rounds
+    //console based
+ }
+
+ function playRound () {
+    
+ }
+  
+ function playerChoice() { 
+    //get input from the player 
+ }
+
+ function computerChoice() {
+  return choices[Math.floor(Math.random()*choices.length)]  
+}
