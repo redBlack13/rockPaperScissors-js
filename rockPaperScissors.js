74b4d5dc999f63a1b2085ffa0546ed756c2b0fbd
Red Black13 <149127977+redBlack13@users.noreply.github.com>
2024-01-03 20:18:50 +1100

    created and completed function playRound() created and completed function checkWinner() both functions tested and appear to be working correctly

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -4,9 +4,32 @@
 
  function getComputerChoice() {
    const choice = options[Math.floor(Math.random() * options.length)];
-   console.log(choice);
-   
+   return choice;
  }
  
- getComputerChoice();
- 
+ function playRound(playerChoice, computerChoice) { 
+   const result = checkWinner(playerChoice, computerChoice);
+   if (result == "Tie") { 
+      return "its a tie";
+   }
+    else if (result == "You Win!") { 
+      return `You Win! $(playerChoice) beats $(computerChoice)`;
+    }
+}
+
+
+
+function checkWinner(playerChoice, computerChoice) {
+   if (playerChoice === computerChoice){
+      return "Tie";
+   } 
+   else if (
+   (playerChoice == "rock" && computerChoice == "scissors") ||
+   (playerChoice == "paper" && computerChoice == "rock") ||
+   (playerChoice == "scissors" && computerChoice == "paper") 
+  ){  
+   return "You Win!";
+   }
+  else {
+      return "Computer Wins" }
+  }
